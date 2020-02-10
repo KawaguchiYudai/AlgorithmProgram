32e8b246584e3b01f8640d686631c30d2a937fdc
KawaguchiYudai <ykawaguchi.1227@gmail.com>
2020-02-11 01:48:23 +0900

    add vecter variable

---
+++ AlgorithmProgram/AlgorithmProgram/Vector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram.Physics
+{
+    class Vector
+    {
+        private float x;
+        public float X => x;
+        private float y;
+        public float Y => y;
+        private float z;
+        public float Z => z;
+
+        public Vector(float _x =0, float _y=0, float _z=0)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+    }
+}
